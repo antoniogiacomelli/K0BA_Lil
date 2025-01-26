5c3870a74d4efab738b3b49665fe766f4d8daa70
antoniogiacomelli <62488903+antoniogiacomelli@users.noreply.github.com>
2025-01-25 23:57:10 -0300

    VER031

--- Inc/kutils.h
+++ Inc/kutils.h
@@ -5,23 +5,28 @@
  ******************************************************************************
  ******************************************************************************
  *  In this header:
- *                  o It is called 'utils' but not really.
+ *                  o Misc utils
  *****************************************************************************/
 
 #ifndef K_UTILS_H
 #define K_UTILS_H
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-
 ULONG kStrLen(STRING s);
 ULONG kMemCpy(ADDR destPtr, ADDR const srcPtr, ULONG size);
+
+#ifdef K_DEF_PRINTF
 
 extern UART_HandleTypeDef huart2;
 int _write(int file, char* ptr, int len);
 
+#endif
+
 #ifdef __cplusplus
  }
 #endif
+
 #endif
