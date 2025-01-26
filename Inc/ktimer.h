300697c74bebcda2afa2d0a729da4be16fccb3da
antoniogiacomelli <62488903+antoniogiacomelli@users.noreply.github.com>
2025-01-25 23:51:12 -0300

    VER031

--- Inc/ktimer.h
+++ Inc/ktimer.h
@@ -7,39 +7,42 @@
 #define RELOAD      1
 #define ONESHOT     0
 
-
 extern K_TIMER *dTimReloadList; /* periodic timers */
 extern K_TIMER *dTimOneShotList; /* one-shot timers */
 extern K_TIMER *dTimSleepList; /* sleep delay list */
 
 extern K_TIMEOUT_NODE* timeOutListHeadPtr ;
-VOID kTimeOut(K_TIMEOUT_NODE *timeOutNode, TICK timeout);
-VOID kRemoveTaskFromMbox(ADDR kobj);
-void kRemoveTaskFromSema(void *kobj);
-VOID kRemoveTaskFromMutex(ADDR kobj);
-VOID kRemoveTaskFromQueue(ADDR kobj);
-BOOL kHandleTimeoutList(void);
-VOID kRemoveTaskFromEvent(ADDR kobj);
+VOID kTimeOut(K_TIMEOUT_NODE, TICK);
+VOID kRemoveTaskFromMbox(ADDR);
+VOID kRemoveTaskFromSema(ADDR);
+VOID kRemoveTaskFromMutex(ADDR);
+VOID kRemoveTaskFromQueue(ADDR);
+BOOL kHandleTimeoutList(VOID);
+VOID kRemoveTaskFromEvent(ADDR);
 
 extern struct kRunTime runTime; /* record of run time */
 
-K_ERR kTimerInit(STRING timerName, TICK const ticks, CALLOUT const funPtr,
-		ADDR const argsPtr, BOOL const reload);
+K_ERR kTimerInit(STRING, TICK const, CALLOUT const,
+		ADDR const, BOOL const);
 
-VOID kBusyDelay(TICK const delay);
+VOID kBusyDelay(TICK const);
 
 #define BUSY(t) kBusyDelay(t)
 
-VOID kSleep(TICK const ticks);
+VOID kSleep(TICK const);
 
 #define SLEEP(t) kSleep(t)
 
 TICK kTickGet(VOID);
 
-BOOL kTimerHandler(void);
+BOOL kTimerHandler(VOID);
 
 K_ERR kTimerPut(K_TIMER* const);
+
 #if (K_DEF_SCH_TSLICE==OFF)
+
 VOID kSleepUntil(TICK const);
+
 #endif
+
 #endif
