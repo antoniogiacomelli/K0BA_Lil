238a8a28e0e1a74d16ae2dada0ff353257a5264c
antoniogiacomelli <62488903+antoniogiacomelli@users.noreply.github.com>
2025-03-05 12:16:52 -0300

    VER031

--- Inc/kitc.h
+++ Inc/kitc.h
@@ -19,9 +19,45 @@ extern "C" {
 
 K_ERR kTaskPend( TICK);
 K_ERR kTaskSignal( K_TASK* const);
+
+#if(K_DEF_MUTEX==ON)
 K_ERR kMutexLock( K_MUTEX* const, TICK tmeout);
 VOID kMutexUnlock( K_MUTEX* const);
+#endif
+/***************************************************************************/
+/* CONDITION VARIABLES                                                     */
+/***************************************************************************/
 
+#if ((K_DEF_MUTEX==ON) && (K_DEF_EVENT==ON))
+VOID kEventSignal( K_EVENT *);
+VOID kEventWake( K_EVENT *);
+K_ERR kEventSleep( K_EVENT *, TICK );
+
+/* this is a helper for condition variables to perform the wait atomically
+ unlocking the mutex and going to sleep
+ for signal and wake one can use EventSignal and EventWake */
+inline K_ERR kCondVarWait( K_EVENT *eventPtr, K_MUTEX *mutexPtr, TICK timeout)
+{
+	K_ERR err;
+	/* atomic */
+	K_CR_AREA
+	K_CR_ENTER
+	kMutexUnlock( mutexPtr);
+	err = kEventSleep( eventPtr, timeout);
+	K_CR_EXIT
+	return (err);
+	/* upon returning (after wake) the condition variable must loop lock the
+	 * the mutex and loop around the condition */
+}
+inline VOID kCondVarSignal( K_EVENT *eventPtr)
+{
+	return (kEventSignal( eventPtr));
+}
+inline VOID kCondVarBroad( K_EVENT *eventPtr)
+{
+	return (kEventWake( eventPtr));
+}
+#endif
 #ifdef __cplusplus
 }
 #endif
